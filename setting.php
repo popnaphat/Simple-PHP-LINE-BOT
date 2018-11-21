c1ffb0a5308083e1818632f6f0ea649fffaa34d3
popnaphat <40283665+popnaphat@users.noreply.github.com>
2018-11-21 15:27:46 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,11 +2,11 @@
 
 class Setting {
 	public function getChannelAccessToken(){
-		$channelAccessToken = "";
+		$channelAccessToken = "A2vi8pE1X2HAQIM8goe1u+naF4fjT21H60PhWLovTesoSjtJrm2pFXqDZsfKHzj0d+Ly1TvYtrZQfZOI++JbMGgZuWZE+5/GWhPQpgCiVhFYq3jv89lB55itG8s7y1msC0wxu2ZeMArAr5KTuCUj2gdB04t89/1O/w1cDnyilFU=";
 		return $channelAccessToken;
 	}
 	public function getChannelSecret(){
-		$channelSecret = "";
+		$channelSecret = "6b22c4107de7c703fea210a87981ef3a";
 		return $channelSecret;
 	}
 	public function getApiReply(){
